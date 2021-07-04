318c13d26563fda1513e088780fbfa09a813ed2c
Crucial <nich.otieno@gmail.com>
2021-07-04 23:07:37 +0300

    Retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "2fb188ff2c6d4b10bd223d3173718cda"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
