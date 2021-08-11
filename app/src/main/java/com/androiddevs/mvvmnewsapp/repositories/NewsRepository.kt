66ab9cca4c0d76146773dd44808e79352868d951
Crucialjun <nich.otieno@gmail.com>
2021-08-11 20:09:39 +0300

    load breaking news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repositories/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repositories/NewsRepository.kt
@@ -1,7 +1,10 @@
 package com.androiddevs.mvvmnewsapp.repositories
 
+import com.androiddevs.mvvmnewsapp.api.RetrofiInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 
 class NewsRepository(val db : ArticleDatabase) {
 
+    suspend fun getBreakingNews(countryCode : String , pageNumber : Int) =
+        RetrofiInstance.api.getBreakingNews(countryCode,pageNumber)
 }
